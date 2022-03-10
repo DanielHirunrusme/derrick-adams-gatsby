e3fa20b7787732bbda8d3c5ec1479b37bd64d3be
Daniel Hirunrusme <daniel@halfhalf.us>
2022-03-10 14:15:04 -0500

    adjust ar

--- src/components/modelViewer.js
+++ src/components/modelViewer.js
@@ -7,17 +7,17 @@ const ModelViewer = () => {
     <model-viewer
       className={s.modelViewer}
       camera-controls
+      bounds="tight" 
       auto-rotate
       disable-zoom
       autoplay
       shadow-intensity="1"
-      ar
-      ar-scale="fixed"
       poster="/poster.webp"
       src="/spring-motion-ar-processed.glb"
       environment-image="neutral"
       alt="A 3D model of Derrick Adams Spring Rider"
       style={{ backgroundColor: "unset", width: "100%", height: "100%" }}
+      ar 
       ar-modes="webxr scene-viewer quick-look"
     >
       <div className="progress-bar hide" slot="progress-bar">
