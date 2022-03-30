f18afa830edc73e23f99e0414426639ea27306c0
Daniel Hirunrusme <daniel@halfhalf.us>
2022-03-29 23:25:34 -0400

    latest

--- src/pages/coming-soon.js
+++ src/pages/coming-soon.js
@@ -14,6 +14,10 @@ import * as s from "../css/index.module.css"
 import loadable from "@loadable/component"
 const ModelViewer = loadable(() => import("../components/modelViewer"))
 
+const getRandDelay = () => {
+  return `${Math.random() * (2 - 1) + 0.15}s`
+}
+
 export default function ComingSoon({ data }) {
   const [isWindow, setIsWindow] = useState(false)
 
@@ -27,31 +31,127 @@ export default function ComingSoon({ data }) {
     <main>
       <Marquee gradient={false}>
         <span className={s.marquee}>
-          Coming Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;Coming
-          Soon&nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;
+          Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞
         </span>
       </Marquee>
+      {/* <h1 className={s.titleComingSoonTop}>
+        <strong>DERRICK ADAMS</strong></h1> */}
       <header className={s.header}>
         <h1 className={s.titleComingSoon}>
-          <strong>FUNTIME UNICORN</strong> by <strong>DERRICK ADAMS</strong>
+          <strong>
+            <span style={{ animationDelay: getRandDelay() }} className={s.blue}>
+              F
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.green}
+            >
+              U
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.yellow}
+            >
+              N
+            </span>
+            <span style={{ animationDelay: getRandDelay() }} className={s.red}>
+              T
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.black}
+            >
+              I
+            </span>
+            <span style={{ animationDelay: getRandDelay() }} className={s.pink}>
+              M
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.purple}
+            >
+              E
+            </span>
+            <span>&nbsp;</span>
+            <span style={{ animationDelay: getRandDelay() }} className={s.blue}>
+              U
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.green}
+            >
+              N
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.yellow}
+            >
+              I
+            </span>
+            <span style={{ animationDelay: getRandDelay() }} className={s.red}>
+              C
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.black}
+            >
+              O
+            </span>
+            <span style={{ animationDelay: getRandDelay() }} className={s.pink}>
+              R
+            </span>
+            <span
+              style={{ animationDelay: getRandDelay() }}
+              className={s.purple}
+            >
+              N
+            </span>
+          </strong>
+          <strong>
+            &nbsp;
+            <span
+              className={s.black}
+              style={{ animationDelay: getRandDelay() }}
+            >
+              by
+            </span>
+            &nbsp;
+            <span
+              className={s.black}
+              style={{ animationDelay: getRandDelay() }}
+            >
+              DERRICK
+            </span>{" "}
+            <span
+              className={s.black}
+              style={{ animationDelay: getRandDelay() }}
+            >
+              ADAMS
+            </span>
+          </strong>
         </h1>
       </header>
 
-      {isWindow && <ModelViewer mode="coming soon" />}
+      {isWindow && (
+        <div className="relative z-10">
+          <ModelViewer mode="coming soon" />
+        </div>
+      )}
+      <footer className={s.comingSoonMarqueFooter}>
+        <Marquee gradient={false}>
+          <span className={s.marquee}>
+          Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming Soon♞♞Coming
+          Soon♞♞
+          </span>
+        </Marquee>
+      </footer>
     </main>
   )
 }
