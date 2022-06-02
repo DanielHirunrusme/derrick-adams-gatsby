10d71932becc851f4aadfbe8497b5816f92cd3d4
Daniel Hirunrusme <daniel@halfhalf.us>
2022-06-02 15:37:19 -0400

    added video page

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,41 +7,40 @@ exports.onCreateWebpackConfig = ({
   plugins,
   actions,
 }) => {
-  if (process.env.environment === "production") {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /canvas/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
-  } else {
+
+  if (stage === "build-html") {
     actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          "~components": path.resolve(__dirname, "src/components"),
-          "~images": path.resolve(__dirname, "src/images"),
-          "~hooks": path.resolve(__dirname, "src/utils/hooks"),
-        },
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
       },
     })
-
-    if (stage === "develop-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /canvas/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
   }
+
+  // actions.setWebpackConfig({
+  //   resolve: {
+  //     alias: {
+  //       "~components": path.resolve(__dirname, "src/components"),
+  //       "~images": path.resolve(__dirname, "src/images"),
+  //       "~hooks": path.resolve(__dirname, "src/utils/hooks"),
+  //     },
+  //   },
+  // })
+
+  // if (stage === "develop-html") {
+  //   actions.setWebpackConfig({
+  //     module: {
+  //       rules: [
+  //         {
+  //           test: /canvas/,
+  //           use: loaders.null(),
+  //         },
+  //       ],
+  //     },
+  //   })
+  // }
 }
