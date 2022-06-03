d572c9d3d6dad4bd3401bcdf6a16dffce1381e1a
Daniel Hirunrusme <daniel@halfhalf.us>
2022-06-03 12:55:07 -0400

    updated files

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+import "./src/css/base.css";
+
+// Logs when the client route changes
+// exports.onRouteUpdate = ({ location, prevLocation }) => {
+//   console.log("new pathname", location.pathname)
+//   console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+// }
+// // Wraps every page in a component
+// exports.wrapPageElement = ({ element, props }) => {
+//   return <div {...props}>{element}</div>
+// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,163 @@
-import * as React from "react"
+import cx from "classnames"
+import React, { useState, useRef } from "react"
+import Main from "../../static/main_trim_lores.mp4"
+import VR_1 from "../../static/vr_1_lores.mp4"
+import * as s from "../css/index.module.css"
+import ReactPlayer from "react-player"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+export default function IndexPage() {
+  // Refs
+  const mainVideo = useRef()
+  // States
+  const [playing, setPlaying] = useState(false)
+  const [title, setTitle] = useState(true)
+  const [playbackRate, setPlaybackRate] = useState(1)
+  const [mainEnded, setMainEnded] = useState(false)
+  const [vrEnded, setVrEnded] = useState(false)
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+  const onMainEnded = () => {
+    // play random VR video
+    console.log("mainEnded")
+    setMainEnded(true)
+  }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+  const onClick = () => {}
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+  const onMouseDown = () => {
+    setPlaying(true)
+    setTitle(false)
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+    if(!vrEnded) {
+      setPlaying(true)
+    } else {
+      setMainEnded(false);
+      setVrEnded(false);
+      mainVideo.current.player.seekTo(0, 'seconds')
+    }
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+    // if (intervalId) {
+    //   clearInterval(intervalId)
+    //   setIntervalId(0)
+    // }
+  }
 
-// markup
-const IndexPage = () => {
+  const onMouseUp = () => {
+    setPlaying(false)
+    // if (intervalId) {
+    //   clearInterval(intervalId);
+    //   setIntervalId(0);
+    //   return;
+    // }
+    // const newIntervalId = setInterval(() => {
+    //   console.log(mainVideo.current)
+    //   let rwdTime = mainVideo.current.player.getCurrentTime() - .25;
+    //   // let time = mainVideo.player.getCurrentTime() - 1;
+    //   if(rwdTime > 0) {
+    //     console.log('rwdTime', rwdTime);
+    //     mainVideo.current.player.seekTo(rwdTime, 'seconds')
+    //   } else {
+    //     mainVideo.current.player.seekTo(0, 'seconds')
+    //     clearInterval(newIntervalId)
+    //     setIntervalId(0);
+    //   }
+    // }, 30);
+
+    // setIntervalId(newIntervalId);
+  }
+
+  const onVrEnded = () => {
+    setVrEnded(true);
+  }
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main className={s.main}>
+      <button
+        // onClick={onClick}
+        onClick={onMouseDown}
+        // onMouseUp={onMouseUp}
+        className={s.button}
+      >{!vrEnded ? "Play" : "Replay?" }</button>
+      <div className={cx(s.title, { [s.titleHidden]: !title })}>
+        <div>
+          <h1  className={s.h1}>FUNTIME UNICORN</h1>
+          <p className={cx(s.bodyText, s.authorText)}>By Derrick Adams</p>
+        </div>
+      </div>
+      <div className={cx(s.video, { [s.videoHidden]: mainEnded })}>
+        <ReactPlayer
+          ref={mainVideo}
+          onEnded={onMainEnded}
+          width="100%"
+          height="100%"
+          volume={0}
+          playing={playing}
+          playsinline={true}
+          url={Main}
+          muted={true}
+          controls={false}
+        />
+      </div>
+      <div className={cx(s.video, { [s.videoHidden]: !mainEnded })}>
+        <ReactPlayer
+          onEnded={onVrEnded}
+          playing={mainEnded}
+          width="100%"
+          height="100%"
+          volume={0}
+          playsinline={true}
+          url={VR_1}
+          muted={true}
+          controls={false}
+        />
+      </div>
+      <div className={cx(s.text, s.bodyText,  { [s.textHidden]: !vrEnded })}>
+        <header className={s.header}>
+          <h1 className={s.h1}>FUNTIME UNICORN</h1>
+          <p className={s.authorText}>By Derrick Adams</p>
+        </header>
+        <nav className={cx(s.nav, s.bodyText)}>
+            <button className={s.bodyText}>About</button>
+            <button className={s.bodyText}>Gallery</button>
+            <button className={s.bodyText}>3D Viewer</button>
+            <button className={s.bodyText}>Share</button>
+          </nav>
+        <div className={s.textContainer}>
+          
+          <section className={s.section}>
+            <article>
+              Derrick Adams’ new edition, brings to life not only the imagery he
+              is known for, but also the bodily experiences it is meant to
+              capture. Each sculpture in the edition is a real-life
+              manifestation of Adams’ signature iconography; a visual vocabulary
+              composed of objects, symbols, colors, and shapes, all of which are
+              recontextualized in order to depict leisure in an unprecedented
+              way while simultaneously illuminating the inherent role
+              consumerism plays in such moments of fun and relief. The edition
+              draws from Adams’ Floaters series, which includes vivid portraits
+              of Black people in various states of rest and play. Mounted atop
+              colorful unicorns or candy shaped plastic floaties popularized in
+              recent years, the figures represent an updated version of
+              contemporary American iconography. Now extended further into the
+              grasp of individuals, FUNTIME UNICORN provides a new way to
+              experience the emotions evoked by Adams’ paintings.
+            </article>
+            <article>
+              This is the gallery section
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+            </article>
+            <article>
+              This is the 3D Viewer section
+            </article>
+            <article>
+              This is the share section
+            </article>
+          </section>
+        </div>
+      </div>
     </main>
   )
 }
-
-export default IndexPage
